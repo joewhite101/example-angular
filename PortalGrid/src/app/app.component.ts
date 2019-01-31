2c87bf785ad155412bd42e5e884fbdc9d99bfd8d
Pietro Di Zanno <pie.dizanno@gmail.com>
2019-01-31 15:11:29 +0100

    User url from entado

--- PortalGrid/src/app/app.component.ts
+++ PortalGrid/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  AfterViewInit {
+
   title = 'PortalGrid';
+  userUrl;
+
+  ngAfterViewInit(): void {
+    this.userUrl = document.getElementById('userUrl').innerHTML.trim();
+  }
 }
