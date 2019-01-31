25e49978a46375076ed86f78309669671356ae83
alessandro <alessandro@alessandro-XPS-13-9370>
2019-01-31 11:49:39 +0100

    getPortals

--- entandoPortalApp/src/main/java/com/cdp/model/Portal.java
+++ entandoPortalApp/src/main/java/com/cdp/model/Portal.java
@@ -4,4 +4,21 @@ package com.cdp.model;
  * Created by josephwhite on 1/31/19.
  */
 public class Portal {
+	
+	private String id;
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
--- entandoPortalApp/src/main/java/com/cdp/services/MockJsonService.java
+++ entandoPortalApp/src/main/java/com/cdp/services/MockJsonService.java
@@ -1,7 +1,39 @@
 package com.cdp.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+import com.cdp.model.Portal;
+
 /**
  * Created by josephwhite on 1/31/19.
  */
 public class MockJsonService {
+	
+	//private static final Logger log = LoggerFactory.getLogger(MockJsonService.class);
+
+    public List<Portal> getPortals() {
+        RestTemplate restTemplate = new RestTemplate();
+        //List<Portal> = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Portal.class);
+        //log.info(quote.toString());
+        
+        Portal financialPortal = new Portal();
+        financialPortal.setId("1");
+        financialPortal.setName("Financial Portal");
+        
+        Portal mortgagePortal = new Portal();
+        mortgagePortal.setId("2");
+        mortgagePortal.setName("Mortgage Portal");
+        
+        List<Portal> portals = new ArrayList<Portal>();
+        portals.add(financialPortal);
+        portals.add(mortgagePortal);
+        return portals;
+        
+    }
+	
 }
