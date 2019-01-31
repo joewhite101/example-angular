d7a49c46d38dd9ffbbc1ebf425e7c4b48a065271
alessandro <alessandro@alessandro-XPS-13-9370>
2019-01-31 11:56:25 +0100

    More ignores and getPortals

--- entandoPortalApp/src/main/java/com/cdp/services/MockJsonService.java
+++ entandoPortalApp/src/main/java/com/cdp/services/MockJsonService.java
@@ -15,23 +15,55 @@ import com.cdp.model.Portal;
 public class MockJsonService {
 	
 	//private static final Logger log = LoggerFactory.getLogger(MockJsonService.class);
-
+/**
+ * 
+/posts	100 posts
+/comments	500 comments
+/albums	100 albums
+/photos	5000 photos
+/todos	200 todos
+/users
+ * @return
+ */
+	
+	
     public List<Portal> getPortals() {
         RestTemplate restTemplate = new RestTemplate();
         //List<Portal> = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Portal.class);
         //log.info(quote.toString());
         
-        Portal financialPortal = new Portal();
-        financialPortal.setId("1");
-        financialPortal.setName("Financial Portal");
+        Portal posts = new Portal();
+        posts.setId("1");
+        posts.setName("Posts");
         
-        Portal mortgagePortal = new Portal();
-        mortgagePortal.setId("2");
-        mortgagePortal.setName("Mortgage Portal");
+        Portal comments = new Portal();
+        comments.setId("2");
+        comments.setName("Comments");
+        
+        Portal albums = new Portal();
+        albums.setId("3");
+        albums.setName("Albums");
+        
+        Portal photos = new Portal();
+        photos.setId("4");
+        photos.setName("Photos");
+        
+        Portal todos = new Portal();
+        todos.setId("5");
+        todos.setName("Todos");
+        
+        Portal users = new Portal();
+        users.setId("6");
+        users.setName("Users");
         
         List<Portal> portals = new ArrayList<Portal>();
-        portals.add(financialPortal);
-        portals.add(mortgagePortal);
+        portals.add(posts);
+        portals.add(comments);
+        portals.add(albums);
+        portals.add(photos);
+        portals.add(todos);
+        portals.add(users);
+        
         return portals;
         
     }
