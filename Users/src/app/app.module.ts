2c2be44c89137b63a43e9e9450199b80224a0c23
Pietro Di Zanno <pie.dizanno@gmail.com>
2019-02-01 15:17:05 +0100

    Set interceptor

--- Users/src/app/app.module.ts
+++ Users/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UsersTableComponent } from './users-table/users-table.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UserShowComponent } from './user-show/user-show.component';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './services/users.service';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptorService } from './utility/auth-interceptor.utility';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,13 @@ import { UsersService } from './services/users.service';
     AppRoutingModule
   ],
   providers: [
-    UsersService
+    UsersService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
