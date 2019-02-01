483732ad23cb6b9b149ce38fd3d5809432ba4c1d
Pietro Di Zanno <pie.dizanno@gmail.com>
2019-02-01 12:08:44 +0100

    fix base url

--- Users/src/app/users.service.ts
+++ Users/src/app/users.service.ts
@@ -4,21 +4,19 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class UsersService implements OnInit {
+export class UsersService {
   // baseUrl = 'http://localhost:8080/exampleAngular/api/portalDemo/';
   // baseUrl = 'http://10.132.37.183:8080/exampleAngular/api/portalDemo/';
-   baseUrl: string;
+  baseUrl: string;
   constructor(private httpClient: HttpClient) { }
 
-  ngOnInit(): void {
-    this.baseUrl = window['PROPERTY']['baseUrl'];
-  }
   getAll(): Observable<ResponseUsers> {
     return this.httpClient.get<ResponseUsers>(this.baseUrl + '/api/portalDemo/users');
   }
 
   getById(id: number): Observable<ResponseUser> {
-    console.log('windows', this.baseUrl);
+    console.log('windows second', window);
+    this.baseUrl = window['PROPERTY']['baseUrl'];
     console.log('base url', this.baseUrl);
     return this.httpClient.get<ResponseUser>(this.baseUrl + '/api/portalDemo/users/' + id);
   }
